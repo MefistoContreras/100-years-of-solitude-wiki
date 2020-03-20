c2007a004fed2ff2ac43ed62c5d86236686af963
MefistoContreras <carlosteiker@gmail.com>
2020-03-19 20:33:31 -0600

    navbar done

---
+++ assets/app.js
@@ -0,0 +1,13 @@
+//Variables
+const familyTree = document.getElementById('family-tree-container');
+
+//eventListeners
+
+    //Detects if someOneClicks on the family tree icon
+    familyTree.addEventListener('click', familyTreeOpens);
+
+//Functions
+
+function familyTreeOpens(e){
+    e.preventDefault();
+}
